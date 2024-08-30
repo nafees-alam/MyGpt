77fe3b6f6a7e09f74fdc85b6f2d5773dfb550b84
nafees-alam <nafeesalamofficial@gmail.com>
2024-08-30 16:19:30 +0530

    deployed the backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://my-gpt-backend-iota.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
